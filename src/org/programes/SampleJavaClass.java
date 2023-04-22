d66954932ab3a3437715aefa0405c8b6bdc26cc0
thangarajd <thangarajd>
2023-04-22 07:23:27 +0530

    commiting the changes in hellomethod

--- src/org/programes/SampleJavaClass.java
+++ src/org/programes/SampleJavaClass.java
@@ -4,7 +4,16 @@ public class SampleJavaClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		SampleJavaClass sm = new SampleJavaClass();
+		sm.HelloMethod();
 
+	}
+	
+	
+	public void HelloMethod() {
+		
+		System.out.println("Hello Method");
 	}
 
 }
